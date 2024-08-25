fb4ad4390383f76ef7de09c242844aeb79f14f18
Anh Tu Chau <anhtuchau99@gmail.com>
2024-08-24 21:07:13 -0400

    Finish add place frontend

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import { validate } from "../../util/validators";
 import "./Input.css";
@@ -30,6 +30,13 @@ export default function Input(props) {
     isTouched: false,
     isValid: false,
   });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    props.onInput(props.id, inputState.value, inputState.isValid);
+  }, [id, onInput, value, isValid]);
 
   const changeHandler = (event) => {
     dispatch({
